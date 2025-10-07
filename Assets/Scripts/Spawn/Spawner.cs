57b5a0fc594f97a392b6b3b0e3382aa545ce3189
Andrea <andreaprezp@gmail.com>
2025-10-07 17:02:28 +0200

    Implement Spawner and SpawnConfig

---
+++ Assets/Scripts/Spawn/Spawner.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private SpawnConfig _spawnConfig;
+    [SerializeField] private List<Transform> _spawnPoints;
+
+    private float _elapsedTime;
+
+    void Start()
+    {
+        ValidateReferences();
+    }
+
+    private void ValidateReferences()
+    {
+        if (_spawnConfig == null)
+            Debug.LogError("Spawn configuration is null. Please reference a SpawnConfig in the Spawner");
+
+        if (_spawnPoints == null || _spawnPoints.Count == 0)
+            Debug.LogError("List of spawn points is empty. Please reference at least one SpawnPoint in the Spawner");
+    }
+
+    void Update()
+    {
+        if (_spawnConfig == null || _spawnPoints == null || _spawnPoints.Count == 0)
+            return;
+
+        if (_elapsedTime < _spawnConfig.SpawnFrequencyInSeconds)
+        {
+            _elapsedTime += Time.deltaTime;
+            return;
+        }
+
+        _elapsedTime = 0;
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        if (_spawnConfig.SpawnMultipleInSamePoint)
+        {
+            var spawnPointIndex = Random.Range(0, _spawnPoints.Count);
+            for (var i = 0; i < _spawnConfig.SpawnQuantity; i++)
+            {
+                var prefabIndex = Random.Range(0, _spawnConfig.PrefabsToSpawn.Count);
+                Instantiate(_spawnConfig.PrefabsToSpawn[prefabIndex], _spawnPoints[spawnPointIndex].position, Quaternion.identity);
+            }
+        }
+        else
+        {
+            for (var i = 0; i < _spawnConfig.SpawnQuantity; i++)
+            {
+                var spawnPointIndex = Random.Range(0, _spawnPoints.Count);
+                var prefabIndex = Random.Range(0, _spawnConfig.PrefabsToSpawn.Count);
+                Instantiate(_spawnConfig.PrefabsToSpawn[prefabIndex], _spawnPoints[spawnPointIndex].position, Quaternion.identity);
+            }
+        }
+    }
+}
