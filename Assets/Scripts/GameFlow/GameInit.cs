75b482e3c89b384294bcd3b78a3bd3509d9b523b
Andrea <andreaprezp@gmail.com>
2025-10-09 17:57:44 +0200

    Implement GameInit

---
+++ Assets/Scripts/GameFlow/GameInit.cs
@@ -0,0 +1,23 @@
+﻿using TowerDefense.Service;
+using UnityEngine;
+
+namespace TowerDefense.GameFlow
+{
+    public class GameInit : MonoBehaviour
+    {
+        private GameFlowService _gameFlowService;
+
+        private void Start()
+        {
+            RegisterServices();
+
+            _gameFlowService = ServiceLocator.GetService<GameFlowService>();
+            _gameFlowService.StartGame();
+        }
+
+        private void RegisterServices()
+        {
+            ServiceLocator.RegisterService<GameFlowService>(new GameFlowService());
+        }
+    }
+}
