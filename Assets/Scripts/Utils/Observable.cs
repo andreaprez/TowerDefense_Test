ee96c247f553e018ebe86b6c03ed1ae34d1b7d2a
Andrea <andreaprezp@gmail.com>
2025-10-09 20:14:10 +0200

    Implement Observable class

---
+++ Assets/Scripts/Utils/Observable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Utils
+{
+    public class Observable<T>
+    {
+        private T _value;
+
+        public Observable(T value)
+        {
+            _value = value;
+        }
+
+        public event Action ValueChanged;
+        public T Value {
+            get => _value;
+            set
+            {
+                if (Equals(value, _value))
+                    return;
+                _value = value;
+                ValueChanged?.Invoke();
+            }
+        }
+    }
+}
