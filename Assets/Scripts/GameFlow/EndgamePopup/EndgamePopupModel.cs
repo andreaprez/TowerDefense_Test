5f826252fb542641ed165007d780b6f4f5461c9c
Andrea <andreaprezp@gmail.com>
2025-10-09 23:44:17 +0200

    Implement MVP for EndgamePopup

---
+++ Assets/Scripts/GameFlow/EndgamePopup/EndgamePopupModel.cs
@@ -0,0 +1,9 @@
+﻿using Utils;
+
+namespace TowerDefense.GameFlow.EndgamePopup
+{
+    public class EndgamePopupModel
+    {
+        public readonly Observable<string> Message = new("");
+    }
+}
---
+++ Assets/Scripts/GameFlow/EndgamePopup/EndgamePopupView.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TowerDefense.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TowerDefense.GameFlow.EndgamePopup
+{
+    public class EndgamePopupView : PopupView
+    {
+        [SerializeField] private Text _messageText;
+        [SerializeField] private Button _menuButton;
+        [SerializeField] private Button _restartButton;
+
+        public event Action MenuButtonPressed;
+        public event Action RestartButtonPressed;
+
+        public override void Show()
+        {
+            _menuButton.onClick.AddListener(OnMenuButtonPressed);
+            _restartButton.onClick.AddListener(OnRestartButtonPressed);
+            base.Show();
+        }
+
+        public override void Hide()
+        {
+            _menuButton.onClick.RemoveListener(OnMenuButtonPressed);
+            _restartButton.onClick.RemoveListener(OnRestartButtonPressed);
+            base.Hide();
+        }
+
+        public void OnMessageChanged(string message)
+        {
+            _messageText.text = message;
+        }
+
+        private void OnMenuButtonPressed()
+        {
+            MenuButtonPressed?.Invoke();
+        }
+
+        private void OnRestartButtonPressed()
+        {
+            RestartButtonPressed?.Invoke();
+        }
+    }
+}
