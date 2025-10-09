f8c6395c52006e3028ad0d3642b84f317a292384
Andrea <andreaprezp@gmail.com>
2025-10-09 17:57:14 +0200

    Implement GameFlowService

---
+++ Assets/Scripts/GameFlow/GameFlowService.cs
@@ -0,0 +1,28 @@
+﻿using TowerDefense.Service;
+using UnityEngine;
+
+namespace TowerDefense.GameFlow
+{
+    public class GameFlowService : IService
+    {
+        public GameState GameState => _gameState;
+        private GameState _gameState;
+
+        public void Init() { }
+        public void Dispose() { }
+
+        public void StartGame()
+        {
+            _gameState = GameState.Gameplay;
+        }
+
+        public void EndGame(bool isWin)
+        {
+            _gameState = GameState.Endgame;
+            if (isWin)
+                Debug.Log("Win!"); //TODO: Show win
+            else
+                Debug.Log("Lose!"); //TODO: Show lose
+        }
+    }
+}
