39321970562055ce6e09a57053683ac5bdb64ba2
Andrea <andreaprezp@gmail.com>
2025-10-10 14:01:34 +0200

    Implement base Turret and TurretRegular

---
+++ Assets/Scripts/Player/Turrets/TurretRegular.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace TowerDefense.Player
+{
+    public class TurretRegular : Turret
+    {
+        protected override void Attack()
+        {
+            var bullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity, transform);
+            bullet.SetTeam(_teamTag)
+                .SetSpeed(_turretConfig.BulletSpeed)
+                .SetLifeTime(_turretConfig.BulletLifeTime)
+                .SetDamage(_turretConfig.Damage)
+                .SetTarget(_closestTarget.Item2);
+        }
+    }
+}
