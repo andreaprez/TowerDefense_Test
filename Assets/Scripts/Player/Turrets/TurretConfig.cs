1d643d02aaf75de905df36e17275087d28c53b6a
Andrea <andreaprezp@gmail.com>
2025-10-10 14:01:02 +0200

    Add properties to TurretConfig

--- Assets/Scripts/Player/Turrets/TurretConfig.cs
+++ Assets/Scripts/Player/Turrets/TurretConfig.cs
@@ -5,7 +5,10 @@ namespace TowerDefense.Player
     [CreateAssetMenu(menuName = "Tower Defense/Turret Config")]
     public class TurretConfig : ScriptableObject
     {
+        public float Range;
         public float FireRateInSeconds;
+        public float BulletSpeed;
+        public float BulletLifeTime;
         public int Damage;
     }
 }
