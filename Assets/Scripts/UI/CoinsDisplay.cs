cef9fe1be38f9c3f300bbeb9108465d5b8a15efb
Andrea <andreaprezp@gmail.com>
2025-10-11 00:33:07 +0200

    Implement CoinsDisplay

---
+++ Assets/Scripts/UI/CoinsDisplay.cs
@@ -0,0 +1,34 @@
+﻿using TMPro;
+using TowerDefense.Currency;
+using TowerDefense.Service;
+using TowerDefense.Signals;
+using UnityEngine;
+
+namespace TowerDefense.UI
+{
+    public class CoinsDisplay : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _coinsText;
+
+        private SignalService _signalService;
+
+        private void Start()
+        {
+            _signalService = ServiceLocator.GetService<SignalService>();
+            _signalService.GetSignal<CoinsUpdatedSignal>().AddListener(OnCoinsUpdated);
+
+            var currencyService = ServiceLocator.GetService<CurrencyService>();
+            OnCoinsUpdated(currencyService.Coins);
+        }
+
+        private void OnDestroy()
+        {
+            _signalService.GetSignal<CoinsUpdatedSignal>().RemoveListener(OnCoinsUpdated);
+        }
+
+        private void OnCoinsUpdated(int coins)
+        {
+            _coinsText.SetText(coins.ToString());
+        }
+    }
+}
