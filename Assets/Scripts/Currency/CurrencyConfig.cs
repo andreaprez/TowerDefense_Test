d807e8b7b186adb278d059123404170f4a0b73a0
Andrea <andreaprezp@gmail.com>
2025-10-11 00:30:52 +0200

    Implement CurrencyService with its config and signal

---
+++ Assets/Scripts/Currency/CurrencyConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace TowerDefense.Currency
+{
+    [CreateAssetMenu(menuName = "Tower Defense/Currency Config")]
+    public class CurrencyConfig : ScriptableObject
+    {
+        public int InitialCoins;
+    }
+}
---
+++ Assets/Scripts/Currency/CurrencyService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TowerDefense.Service;
+using TowerDefense.Signals;
+
+namespace TowerDefense.Currency
+{
+    public class CurrencyService : IService
+    {
+        private readonly CurrencyConfig _currencyConfig;
+        private SignalService _signalService;
+
+        public int Coins => _coins;
+        private int _coins;
+
+        public CurrencyService(CurrencyConfig currencyConfig)
+        {
+            _currencyConfig = currencyConfig;
+        }
+
+        public void Init()
+        {
+            _signalService = ServiceLocator.GetService<SignalService>();
+
+            _coins = _currencyConfig.InitialCoins;
+        }
+
+        public void AddCoins(int amount)
+        {
+            _coins += amount;
+            _signalService.GetSignal<CoinsUpdatedSignal>().Send(_coins);
+        }
+
+        public void RemoveCoins(int amount)
+        {
+            _coins -= amount;
+            _coins = Math.Max(_coins, 0);
+            _signalService.GetSignal<CoinsUpdatedSignal>().Send(_coins);
+        }
+    }
+}
