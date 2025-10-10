4bf453c51f00500c236db8d677e5396de7af48f7
Andrea <andreaprezp@gmail.com>
2025-10-10 16:35:10 +0200

    Implement GameplayInputHandler and signals for input

---
+++ Assets/Scripts/Input/GameplayInputHandler.cs
@@ -0,0 +1,51 @@
+﻿using TowerDefense.GameFlow;
+using TowerDefense.Service;
+using TowerDefense.Signals;
+using UnityEngine;
+
+namespace TowerDefense.Input
+{
+    public class GameplayInputHandler : MonoBehaviour
+    {
+        private const int TerrainLayer = 3;
+        private const int TerrainMask = 1 << TerrainLayer;
+
+        private SignalService _signalService;
+        private GameFlowService _gameFlowService;
+        private Camera _camera;
+
+        private void Start()
+        {
+            _signalService = ServiceLocator.GetService<SignalService>();
+            _gameFlowService = ServiceLocator.GetService<GameFlowService>();
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (_gameFlowService.GameState != GameState.Gameplay)
+                return;
+
+            if (UnityEngine.Input.GetKeyUp(KeyCode.Alpha1))
+                ToggleSelectionForRegularTurret();
+
+            if (UnityEngine.Input.GetMouseButtonUp(0))
+                PlaceTurret();
+        }
+
+        private void ToggleSelectionForRegularTurret()
+        {
+            _signalService.GetSignal<ToggledSelectionForRegularTurretSignal>().Send();
+        }
+
+        private void PlaceTurret()
+        {
+            var ray = _camera.ScreenPointToRay(UnityEngine.Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, 1000f, TerrainMask))
+            {
+                var mouseWorldPosition = hit.point;
+                _signalService.GetSignal<PlacedTurretSignal>().Send(mouseWorldPosition);
+            }
+        }
+    }
+}
